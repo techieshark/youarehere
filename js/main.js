0ebc4e2673736903ed826ec45e12e405a9a19fdd
Peter W <techieshark@gmail.com>
2014-03-26 17:14:16 -0700

    code cleanup
    Removed vanilla Geolocation API code in favor of geoPosition.js which
    works across broader set of browsers, platforms.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,14 @@
-var pos; // DEBUG
-
-function get_location() {
-    if (Modernizr.geolocation) {
-        navigator.geolocation.getCurrentPosition(use_position, handle_geoloc_error);
-
-    } else {
-        // no native support; maybe try a fallback?
-    }
+// p : geolocation object
+function geoloc_success(p){
+    console.log("We found you! At lat: " + p.coords.latitude + ", lon: " + p.coords.longitude);
+    console.log("(Within " + p.coords.accuracy + " meters.)");
 }
 
-function use_position(position) {
-    pos = position;
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    console.log("lat: " + latitude + ", lon: " + longitude);
-    // let's show a map or do something interesting!
-}
-
-function handle_geoloc_error(position_error) {
-    console.log(position_error);
-    if (err.code == 1) { //PERMISSION_DENIED
-        // user said no!
+function geoloc_error(p){
+    console.log("Bummer! Geolocation failed: " + p.message);
+    if (p.code == p.PERMISSION_DENIED) { // user said no!
         console.log("Aww, sad. The user didn't want to tell us where they are.");
-    } else if (err.code == 2) { //POSITION_UNAVAILABLE
+    } else if (p.code == p.POSITION_UNAVAILABLE) {
         console.log("Bummer! A network or GPS error prevented us from discovering user's location.");
     }
 }
@@ -35,40 +21,15 @@ $(document).ready(function() {
         FB.init({
           appId: '550788961695418',
         });
-        // $('#loginbutton,#feedbutton').removeAttr('disabled');
-        // FB.getLoginStatus(updateStatusCallback);
     });
-
-    // get_location();
 
     // What is the user's location?
     if(geoPosition.init()){  // Geolocation Initialisation
-            geoPosition.getCurrentPosition(success_callback,error_callback,{enableHighAccuracy:true});
+            geoPosition.getCurrentPosition(geoloc_success,geoloc_error,{enableHighAccuracy:true});
     } else {
             // You cannot use Geolocation in this device
             //console.log("This device doesn't support Geolocation.");
     }
     // geoPositionSimulator.init();
-
-    // p : geolocation object
-    function success_callback(p){
-
-        console.log("We found you! At lat: " + p.coords.latitude + ", lon: " + p.coords.longitude);
-        console.log("(Within " + p.coords.accuracy + " meters.)");
-        // p.latitude : latitude value
-        // p.longitude : longitude value
-    }
-
-    function error_callback(p){
-        console.log("Bummer! Geolocation failed: " + p.message);
-        if (p.code == p.PERMISSION_DENIED) { //PERMISSION_DENIED
-            // user said no!
-            console.log("Aww, sad. The user didn't want to tell us where they are.");
-        } else if (p.code == p.POSITION_UNAVAILABLE) { //POSITION_UNAVAILABLE
-            console.log("Bummer! A network or GPS error prevented us from discovering user's location.");
-        }
-
-        // p.message : error message
-    }
 
 });
