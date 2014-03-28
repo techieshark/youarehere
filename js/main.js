e35cb2b9f7bc1a761daeedc92649a19142a5d442
Peter W <techieshark@gmail.com>
2014-03-28 01:46:16 -0700

    filter/highlight interesting places (currently: food)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,67 @@
+var glob; // global debug obj
+
+// return true if spot is cool
+function cool(spot) {
+
+    // Also check spot.category_list for interesting categories.
+    console.log(spot.category_list);
+
+    // spot.category_list[i].name
+    var i = _.filter(spot.category_list, function(cat) { return interesting(cat.name); }).length;
+    var j = interesting(spot.category);
+
+    return i || j;
+    // return true;
+}
+
+// return html version of spot
+function htmlprint(spot) {
+    glob = spot.category_list;
+    // return "<li>" + spot.name + ", " + spot.location.street + "</li>"
+    var fun = cool(spot);
+    return "<li>" + (fun?"<b>":"") + spot.name + ", " + spot.category
+    + ': [' + _.map(spot.category_list, function(cat) { return cat.name; }).join(', ') + ']' +
+    (fun?"</b>":"") + "</li>"
+}
+
+function interesting (thing) {
+    var food = /food/i.test(thing);
+    var restaurant = /restaurant/i.test(thing);
+    var pizza = /pizza/i.test(thing);
+    var cafe = /cafe/i.test(thing);
+    var interesting = food || restaurant || pizza || cafe;
+    return interesting;
+}
+
 // p : geolocation object
 function geoloc_success(p){
     console.log("We found you! At lat: " + p.coords.latitude + ", lon: " + p.coords.longitude);
     console.log("(Within " + p.coords.accuracy + " meters.)");
+
+    FB.login(function() {
+        // FB.api(path, method (default: get), params, callback);
+        lat = 33.378766;
+        lon = -111.86182;
+        // lat = p.coords.latitude;
+        // lon = p.coords.longitude;
+        FB.api("/search",
+            {type: 'place', center: '' + lat + ',' + lon, distance: '1000'},
+            function(response) {
+
+                $('#resultlist').replaceWith(
+                    "<ul>" +
+                    _.map(
+                        _.filter(response.data, function(x) { return true; } /*cool*/),
+                        htmlprint
+                    ).join('') +
+                    "</ul>");
+
+                console.log(response); }
+
+                // Filter for:
+                // food, cafe, restaurant, pizza
+        );
+    });
 }
 
 function geoloc_error(p){
@@ -21,13 +81,7 @@ $(document).ready(function() {
         FB.init({
           appId: '550788961695418',
         });
-        FB.login(function() {
-            // FB.api(path, method (default: get), params, callback);
-            FB.api("/search",
-                {type: 'place', center: '33.378766,-111.86182', distance: '1000'},
-                function(response) { console.log(response); }
-            );
-        });
+
 
         // later: only run this after we have both location and facebook api (maybe we
         // can assume that getting the fb api takes no-little time?)
